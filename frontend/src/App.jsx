4d3bd6902a6e2b2705d18bdc2bd7253e90ea46c8
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-03-23 21:18:05 +0530

    navbar created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Doctors from './pages/Doctors'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import MyProfile from './pages/Myprofile'
+import MyAppointment from './pages/MyAppointment'
+import Appointment from './pages/Appointment'
+import {Routes,Route} from 'react-router-dom'
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
+       <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/doctors' element={<Doctors/>}/>
+        <Route path='/doctors/:speciality' element={<Doctors/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/my-profile' element={<MyProfile/>}/>
+        <Route path='/my-appointment' element={<MyAppointment/>}/>
+        <Route path='/appointment/:docId' element={<Appointment/>}/>
+       </Routes>
+    </div>
+  )
+}
+
+export default App
