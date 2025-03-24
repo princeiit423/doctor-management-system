b11260d0e458f1e77e725af9e79efe191f239515
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-03-24 10:39:26 +0530

    speciality component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
         <img src={assets.group_profiles} />
         <p>Simply browse through our extensive list of trusted doctors,<br className='hidden sm:block'/> schedule your appointment hassle-free.</p>
     </div>
-    <a className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm  md: m-0 hover:scale-105 transition-all duration-300' href='#'>
+    <a href="#speciality" className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm  md: m-0 hover:scale-105 transition-all duration-300' >
         Book Appointment <img className='w-3' src={assets.arrow_icon} alt=''/>
     </a>
       </div>
