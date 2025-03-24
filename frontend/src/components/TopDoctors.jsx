e300df9aba766ac83f68fb50cad001aa6a397627
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-03-24 11:33:48 +0530

    banner component

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { doctors } from '../assets/assets'
+import { AppContext } from '../context/AppContext';
 import {useNavigate} from 'react-router-dom'
-
+import { useContext } from 'react';
 const TopDoctors = () => {
     const navigate= useNavigate();
+    const{doctors} = useContext(AppContext)
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-800 md:mx-10'>
       <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
@@ -26,7 +27,7 @@ const TopDoctors = () => {
             ))
         }
     </div>
-    <button className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>More</button>
+    <button onClick={()=>{navigate('/doctors'); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>More</button>
     </div>
   )
 }
