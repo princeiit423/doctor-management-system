a3c3ce0c0ca8057c4046728409b5f426d1c21834
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-03-30 14:00:14 +0530

    navbar of admin done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import Login from './pages/Login'
+import React, { useContext } from "react";
+import Login from "./pages/Login";
+import { AdminContext } from "./context/AdminContext";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return (
-    <div >
-      <Login/>
+  const { aToken } = useContext(AdminContext);
+  return aToken ? (
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <Navbar/>
     </div>
-  )
-}
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
+  );
+};
 
-export default App
+export default App;
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,19 +1,48 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { AdminContext } from '../context/AdminContext.jsx'
 //import {assets} from '../assets/assets'
+import axios from 'axios'
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 const Login = () => {
     const [state,setState] = useState('Admin')
+    const {setAToken,backendUrl} = useContext(AdminContext)
+    const[email,setEmail]= useState('')
+    const [password,setPassword] = useState('')
+
+    const onSubmitHandler= async (event)=>{
+        event.preventDefault()
+        try {
+          if(state === 'Admin' ){
+            console.log(backendUrl + '/api/admin/login')
+              const {data} = await axios.post(backendUrl + '/api/admin/login', {email,password})
+              if(data.success === 'true'){
+                localStorage.setItem('aToken', data.token)
+                setAToken(data.token)
+              } else{
+                toast.error(data.message)
+              }
+          } else{
+
+          }
+        } catch (error) {
+          console.log(error)
+        }
+    }
   return (
-    <form className='min-h-[80vh] flex items-center'>
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
       <div className=' flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
         <p className='text-2xl font-semibold m-auto '><span className='text-[#5f6FFF]'>{state} </span> Login</p>
         <div className='w-full'>
           <p>Email</p>
-          <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type='email' required />
+          <input onChange={(e)=> setEmail(e.target.value)} value={email} className='border border-[#DADADA] rounded w-full p-2 mt-1' type='email' required />
         </div>
 
         <div className='w-full'>
           <p>Password</p>
-          <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type='password' required />
+          <input onChange={(e)=> setPassword(e.target.value)} value={password} className='border border-[#DADADA] rounded w-full p-2 mt-1' type='password' required />
         </div>
         <button className='bg-[#5f6FFF] text-white w-full py-2 rounded-md text-base '>Login</button>
         {
