1de62ad7fdaeee08b52affdb20ae28ca5b335001
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-04-03 12:12:59 +0530

    book apointment API link to frontend

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,64 +1,71 @@
 import { createContext, useEffect, useState } from "react";
-export const AppContext= createContext()
-import axios from 'axios'
-import {toast} from 'react-toastify'
+export const AppContext = createContext();
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export const AppContextProvider = (props) => {
-    const currencySymbol = '$'
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const [doctors,setDoctors]= useState([])
-    const [token,setToken]= useState(localStorage.getItem('token')?localStorage.getItem('token'): false)
-    const [userData,setUserData]= useState(false);
+  const currencySymbol = "$";
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [doctors, setDoctors] = useState([]);
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : false
+  );
+  const [userData, setUserData] = useState(false);
 
-    
-    const getDoctorsData = async ()=>{
-        try {
-            const {data} = await axios.get(backendUrl+'/api/doctor/list')
-            if(data.success === true){
-                setDoctors(data.doctors)
-            } else{
-                toast.error(data.message)
-            }
-        } catch (error) {
-            console.log(error)
-            toast.error(error.message)
-        }
+  const getDoctorsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/list");
+      if (data.success === true) {
+        setDoctors(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
     }
+  };
 
-    const getUserData= async()=>{
-        try {
-            const {data}= await axios.get(backendUrl+'/api/user/get-profile', {headers:{token}})
-            if(data.success === true){
-                console.log(data);
-                setUserData(data.userData)
-            }else{
-                toast.error(data.message)
-            }
-            
-        } catch (error) {
-            console.log(error)
-            toast.error(error.message)
-        
-        }
+  const getUserData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/get-profile", {
+        headers: { token },
+      });
+      if (data.success === true) {
+        console.log(data);
+        setUserData(data.userData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
     }
+  };
 
-    const value={
-        doctors,currencySymbol,backendUrl,token,setToken,getUserData,userData,setUserData
+  const value = {
+    doctors,
+    currencySymbol,
+    backendUrl,
+    token,
+    setToken,
+    getUserData,
+    userData,
+    setUserData,
+    getDoctorsData,
+  };
+
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+      getUserData();
     }
-    
-    useEffect(()=>{
-        getDoctorsData()
-    },[])
-
-    useEffect(()=>{
-        if(token){
-            getUserData();
-        }
-    },[token])
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
-export default AppContextProvider
+  }, [token]);
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+export default AppContextProvider;
--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,13 +1,17 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { AppContext } from '../context/AppContext';
 import { assets } from '../assets/assets';
 import RelatedDoctor from '../components/RelatedDoctor';
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const Appointment = () => {
   const { docId } = useParams();
-  const { doctors, currencySymbol } = useContext(AppContext);
+  const { doctors, currencySymbol,token,backendUrl,getDoctorsData } = useContext(AppContext);
   const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+  const navigate = useNavigate();
 
   const [docInfo, setDocInfo] = useState(null);
   const [docSlots, setDocSlots] = useState([]);
@@ -45,10 +49,22 @@ const Appointment = () => {
       while (currentDate < endTime) {
         let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: "2-digit" });
 
+        let day = currentDate.getDate()
+        let month = currentDate.getMonth()+1
+        let year = currentDate.getFullYear()
+
+        const slotDate = day + "_" + month + "_" + year
+        const slotTime = formattedTime
+
+        const isSlotAvailable = docInfo.slots_booked[slotDate] && docInfo.slots_booked[slotDate].includes(slotTime) ? false : true
+
+        if(isSlotAvailable){
+            
         timeSlots.push({
           datetime: new Date(currentDate),
           time: formattedTime
         });
+        }
 
         currentDate.setMinutes(currentDate.getMinutes() + 30);
       }
@@ -56,6 +72,34 @@ const Appointment = () => {
     }
     setDocSlots(slots);
   };
+
+  const bookAppointment= async ()=>{
+    if(!token){
+      toast.warn("Login to book appointment");
+      navigate('/login');
+    }
+    try {
+      const date = docSlots[slotIndex][0].datetime
+
+      let day = date.getDate()
+      let month = date.getMonth()+1
+      let year = date.getFullYear()
+
+      const slotDate = day + "_" + month + "_" + year
+      //console.log(slotDate);
+      const {data} = await axios.post(backendUrl + '/api/user/book-appointment',{docId, slotDate,slotTime}, {headers:{token}})
+      if(data.success === true){
+        toast.success(data.message);
+        getDoctorsData()
+        navigate('/my-appointments')
+      } else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  }
 
   useEffect(() => {
     fetchDocInfo();
@@ -116,7 +160,7 @@ const Appointment = () => {
             </p>
           ))}
         </div>
-        <button className='bg-[#5f6FFF] text-white text-sm font-light px-14 py-3 mt-5 rounded-full cursor-pointer'>Book an Appointment</button>
+        <button onClick={bookAppointment} className='bg-[#5f6FFF] text-white text-sm font-light px-14 py-3 mt-5 rounded-full cursor-pointer'>Book an Appointment</button>
       </div>
       {/*Listing related doctors  */}
       <RelatedDoctor docId={docId} speciality={docInfo.speciality} />
