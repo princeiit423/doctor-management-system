378c5ab06d3332f164107d3fad1f3bf024d7ac8c
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-04-03 13:10:27 +0530

    cancel appointment route created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -159,4 +159,30 @@ const listAppointment = async (req,res)=>{
     }
 }
 
-export { userRegister , userLogin, getProfile, updateProfile, bookAppointment,listAppointment};
+// API to cancel appointment
+
+const cancelAppointment = async (req,res)=>{
+    try {
+        const {userId, appointmentId}= req.body;
+
+        const appointmentData = await appointmentModel.findById(appointmentId);
+
+        // verify appointment user
+        if( appointmentData.userId !== userId){
+            return res.json({success:false,message:"Unauthorized action"})
+        }
+        await appointmentModel.findByIdAndUpdate(appointmentId,{cancelled:true})
+
+        // releasing doctor slot
+        const {docId, slotDate, slotTime}= appointmentData
+        const doctorData = await doctorModel.findById(docId)
+        let slots_booked = doctorData.slots_booked
+
+        slots_booked[slotDate] = slots_booked[slotDate].filter(e=> e !== slotTime)
+        await doctorModel.findByIdAndUpdate(docId,{slots_booked})
+        res.json({success:true,message:"Appointment cancelled"});
+    } catch (error) {
+        res.json({message:error.message});
+    }
+}
+export { userRegister , userLogin, getProfile, updateProfile, bookAppointment,listAppointment, cancelAppointment};
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {userRegister, userLogin, getProfile, updateProfile, bookAppointment, listAppointment} from '../controllers/userController.js';
+import {userRegister, userLogin, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment} from '../controllers/userController.js';
 import authUser from '../middlewares/authUser.js';
 import upload from '../middlewares/multer.js';
 const userRouter = express.Router();
@@ -10,5 +10,6 @@ userRouter.get('/get-profile', authUser ,getProfile);
 userRouter.post('/update-profile',upload.single('image'), authUser, updateProfile)
 userRouter.post('/book-appointment',authUser,bookAppointment);
 userRouter.get('/appointments',authUser,listAppointment)
+userRouter.post('/cancel-appointment', authUser,cancelAppointment);
 
 export default userRouter;
