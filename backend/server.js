67964c3d1f4aea75a5ddcafc75057a2ae5878f95
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-04-04 21:25:27 +0530

    origin

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,10 @@ connectCloudinary();
 //middlewares
 app.use(express.json());
 app.use(
-  cors({
-    origin: 'https://doc-nest-five.vercel.app',
-    methods: 'GET,POST,PUT,DELETE', // Allow specific HTTP methods
-    allowedHeaders: 'Content-Type,Authorization', // Allow specific headers
-  })
-);
+    cors({
+      origin: '*',
+    })
+  );
   
 //api endpoints
 
