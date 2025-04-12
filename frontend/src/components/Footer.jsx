1f10be715598e256faef6b63974628e9ae6332d3
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-04-12 22:02:35 +0530

    footer change

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => {
       
       <div>
         <hr/>
-        <p className='py-5 text-sm text-center'>Copyright 2024 @ Greatstack.dev - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright 2024 @ Faiz.dev - All Right Reserved.</p>
       </div>
     </div>
   );
