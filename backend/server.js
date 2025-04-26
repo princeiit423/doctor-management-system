b4d1adfddc0eed91e708aca24793647ef1dab478
Faiz Hussain <129678701+princeiit423@users.noreply.github.com>
2025-04-26 23:12:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use('/api/admin', adminRouter)
 
 app.use('/api/doctor',doctorRouter)
 
-app.use('api/user', userRouter)
+app.use('/api/user', userRouter)
 app.get('/', (req,res)=>{
     res.send("API works");
 })
